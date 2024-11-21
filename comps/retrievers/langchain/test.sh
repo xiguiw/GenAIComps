c0c1df3d728b1688dd855e77e4ea691e230a010d
Wang, Xigui <xigui.wang@intel.com>
2024-11-21 17:17:55 +0800

    Add test.sh

---
+++ comps/retrievers/langchain/test.sh
@@ -0,0 +1,7 @@
+export HUGGINGFACEHUB_API_TOKEN=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
+export your_embedding=$(python3 -c "import random; embedding = [random.uniform(-1, 1) for _ in range(768)]; print(embedding)")
+curl http://${host_ip}:7000/v1/retrieval \
+  -X POST \
+  -d "{\"text\":\"test\",\"embedding\":${your_embedding}}" \
+  -H 'Content-Type: application/json'
+unset your_embedding
